a465eb4f5b03dcf00560f44679b7725671b72ddb
DoctorPok <pokdoctor@gmail.com>
2025-11-30 16:35:21 +0100

    Feat(emil & search): add mailhog and meilisearch

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,26 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', '127.0.0.1'),
+        port: env.int('SMTP_PORT', 1025),
+        secure: false,
+        ignoreTLS: true,
+        auth: undefined,
+      },
+      settings: {
+        defaultFrom: env('EMAIL_DEFAULT_FROM', 'no-reply@example.com'),
+        defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'no-reply@example.com'),
+      },
+    },
+  },
+
+  meilisearch: {
+    config: {
+      host: env("MEILISEARCH_HOST", "http://localhost:7700"),
+      apiKey: env("MEILISEARCH_API_KEY"),
+      posts: {},
+    },
+  },
+});
